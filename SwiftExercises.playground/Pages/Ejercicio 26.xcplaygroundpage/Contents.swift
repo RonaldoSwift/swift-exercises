9dbaaf4e8d0f310c2fb3fdd17d71e1085692cfb7
Ronaldo Andre Vargas Huaman <ronaldovh99@gmail.com>
2020-03-02 13:21:08 -0500

    Ejercicio 26

---
+++ SwiftExercises.playground/Pages/Ejercicio 26.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+import Foundation
+print ("Programa que suma la serie de 3 al 99 de 3 en 3")
+var I = 3
+var A = 0
+while I <= 99 {
+    A = A + I
+    I = I + 3
+}
+print ("La suma de las serie es: \(A)")
+
+//: [Next](@next)
