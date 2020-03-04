808a14dd8307725623f08aecdb8c3a4c797e6beb
Ronaldo Andre Vargas Huaman <ronaldovh99@gmail.com>
2020-03-03 19:58:53 -0500

    borrado espacio

---
+++ SwiftExercises.playground/Pages/Ejercicio24.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+import Foundation
+print ("Programa para calcular el mas grande, el pequeño y la media de N numeros")
+let num1 = 0
+let num2 = 0
+print ("Cuantos numeros daras: ")
+let N
+if N > 0 {
+    print ("Dame un numero")
+    let X
+    if (num1 == 0){
+        p = N
+        g = N
+    }
+}
+//: [Next](@next)
---
+++ SwiftExercises.playground/Pages/Ejercicio27.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+import Foundation
+
+print ("Programa para imprimir los impares del 7 al 51")
+var I = 7
+while I <= 51 {
+    var I = I + 2
+    print ("Los numeros impares son \(num1)")
+}
+//: [Next](@next)
