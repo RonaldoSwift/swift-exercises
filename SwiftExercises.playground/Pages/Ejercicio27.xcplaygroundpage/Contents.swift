75e0c13e60ec14a16fc01bacae586fcf4e3c7de7
Ronaldo Andre Vargas Huaman <ronaldovh99@gmail.com>
2020-03-03 20:00:33 -0500

    Ejercicio27

--- SwiftExercises.playground/Pages/Ejercicio27.xcplaygroundpage/Contents.swift
+++ SwiftExercises.playground/Pages/Ejercicio27.xcplaygroundpage/Contents.swift
@@ -5,7 +5,7 @@ import Foundation
 print ("Programa para imprimir los impares del 7 al 51")
 var I = 7
 while I <= 51 {
-    var I = I + 2
-    print ("Los numeros impares son \(num1)")
+     I = I + 2
+    print ("Los numeros impares son \(I)")
 }
 //: [Next](@next)
