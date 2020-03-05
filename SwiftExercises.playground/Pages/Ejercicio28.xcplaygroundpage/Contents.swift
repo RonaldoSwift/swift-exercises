4af1428d925d4af226161a566cebe4cfdad8ab45
Ronaldo Andre Vargas Huaman <ronaldovh99@gmail.com>
2020-03-04 19:42:45 -0500

    ejercicio28

--- SwiftExercises.playground/Pages/Ejercicio28.xcplaygroundpage/Contents.swift
+++ SwiftExercises.playground/Pages/Ejercicio28.xcplaygroundpage/Contents.swift
@@ -1,7 +1,14 @@
 //: [Previous](@previous)
 
 import Foundation
-
-var str = "Hello, playground"
+print ("Programa que verifica si un dato ingresado por teclado corresponde a la contraseña 252525")
+print ("Ingrese digito")
+let num1 = 252525
+if num1 != 252525 {
+    print ("Contraseña incorrecta")
+}
+if num1 == 252525{
+    print ("Contraseña correcta")
+}
 
 //: [Next](@next)
