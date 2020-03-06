820ee715991f52dc382f1ef917d00ef80b09c585
Ronaldo Andre Vargas Huaman <ronaldovh99@gmail.com>
2020-03-06 18:55:20 -0500

    Ejercicio24

--- SwiftExercises.playground/Pages/Ejercicio24.xcplaygroundpage/Contents.swift
+++ SwiftExercises.playground/Pages/Ejercicio24.xcplaygroundpage/Contents.swift
@@ -1,17 +1,35 @@
 //: [Previous](@previous)
 
 import Foundation
-print ("Programa para calcular el mas grande, el pequeño y la media de N numeros")
-let num1 = 0
-let num2 = 0
-print ("Cuantos numeros daras: ")
-let N
-if N > 0 {
-    print ("Dame un numero")
-    let X
-    if (num1 == 0){
-        p = N
-        g = N
-    }
+print("Programa para calcular el mas grande, el pequeño y la media de N numeros")
+var i = 0
+var a = 0
+
+var p = 0
+var g = 0
+
+print("Cuantos numeros daras: ")
+var N = 5
+if N > 0 {  
+    repeat {
+        print ("Ingrese un numero")
+        let x = 4
+        if i == 0 {
+            p = x
+            g = x
+            if x < p {
+                p = x
+            }
+            if x > g {
+                g = x
+            }
+            i = i + 1
+            a = a + 1
+        }
+    } while i >= N
+    N = a / N
+    print("Mayor \(g) menor\(p) medio\(N)")
+} else {
+    print("Error")
 }
 //: [Next](@next)
