c4e549d57f7d9533b465e096427d88a7c870d047
Ronaldo Andre Vargas Huaman <ronaldovh99@gmail.com>
2020-03-06 19:19:11 -0500

    Ejercicio31; obserbacion: se repite varias veces el print("Ingrese numero"); pero el resultado si da.

---
+++ SwiftExercises.playground/Pages/Ejercicio31.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+import Foundation
+print("Programa que te da la suma de 25 numeros")
+var  s = 0
+for i in 1...25 {
+    print("Ingrese un numero")
+    let x = 10
+    s = s + x
+}
+print("La suma es: \(s)")
+//: [Next](@next)
