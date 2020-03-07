0396a9f2d80921a68f24fd5054b17dc46487f287
Ronaldo Andre Vargas Huaman <ronaldovh99@gmail.com>
2020-03-06 19:09:50 -0500

    Ejercicio30

---
+++ SwiftExercises.playground/Pages/Ejercicio30.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+//: [Previous](@previous)
+
+import Foundation
+print("Programa que imprima enteros del 1 al 30 con sus reciprocos")
+for i in 1...30 {//
+    print ("\(i) reciproco \(1/i)")
+}
+//: [Next](@next)
